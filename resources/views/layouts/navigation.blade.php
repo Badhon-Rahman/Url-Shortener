9f0f8770a072a87f865639cc2c33e57d5fede85c
Badhon Rahman <badhon.mostafizur@gmail.com>
2021-11-29 19:07:07 +0600

    update view

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -70,6 +70,9 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('yourLink')" :active="request()->routeIs('yourLink')">
+                {{ __('Your Links') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
